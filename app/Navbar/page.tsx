04318bde6f92663c2894d68b8f9bba14767054e1
serhatacarhello <serhat.acar.tr@gmail.com>
2024-02-09 09:13:35 +0300

    navbar created add logo icon style the active link

---
+++ app/Navbar/page.tsx
@@ -0,0 +1,38 @@
+"use client";
+import classNames from "classnames";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+import { AiFillBug } from "react-icons/ai";
+
+export default function Navbar() {
+  const currentPath = usePathname();
+
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues" },
+  ];
+  return (
+    <nav className="flex space-x-6 border-b shadow-sm mb-5 px-5 h-14 items-center  ">
+      <Link href={"/"}>
+        <AiFillBug />
+      </Link>
+      <ul className="flex  space-x-6 ">
+        {links.map((link) => (
+          <li key={link.href}>
+            <Link
+              href={link.href}
+              className={classNames({
+                "text-zinc-900": link.href === currentPath,
+                "text-zinc-500": link.href !== currentPath,
+                "hover:text-zinc-800 transition-colors": true,
+              })}
+            >
+              {link.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+}
