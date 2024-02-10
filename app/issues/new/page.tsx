35683dd83272465f1e6ff3707359e2302723495b
serhatacarhello <serhat.acar.tr@gmail.com>
2024-02-10 10:48:48 +0300

    Built new issue page

---
+++ app/issues/new/page.tsx
@@ -0,0 +1,18 @@
+
+"use client"
+import { Button, TextArea, TextField, TextFieldInput } from '@radix-ui/themes'
+import React from 'react'
+
+export default function NewIssuePage() {
+  return (
+    <div className='max-w-xl space-y-3'>
+          <TextField.Root>             
+
+              <TextField.Input placeholder='Title'/>
+          </TextField.Root>
+          <TextArea placeholder='Description' />
+
+          <Button>Submit New Issue</Button>
+    </div>
+  )
+}
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,5 +1,9 @@
 import React from "react";
-
+import { Button } from "@radix-ui/themes";
+import Link from "next/link";
 export default function IssuesPage() {
-  return <div>IssuesPage</div>;
+  return <div className="ml-5">
+    <Button><Link href={"/issues/new"}>New Issue
+    </Link> </Button>
+  </div> 
 }
