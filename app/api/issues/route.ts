17b220aae717b1ee273f0860369f0c28dfc2d341
serhatacarhello <serhat.acar.tr@gmail.com>
2024-02-11 15:18:24 +0300

    Handle errors

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -3,8 +3,8 @@ import * as z from "zod"
 import prisma from "@/prisma/db";
 
 const createIssueSchema= z.object({
-    title: z.string().min(1).max(255),
-    description:z.string().min(1)
+    title: z.string().min(1, "Title is required.").max(255),
+    description:z.string().min(1, "Description is required.")
 
 })
 
