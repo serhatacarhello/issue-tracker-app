4571bfea04e097a412a5699fedc4df789c4adaea
serhatacarhello <serhat.acar.tr@gmail.com>
2024-02-12 16:04:15 +0300

    Refactor: Apply the SRP

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -4,6 +4,8 @@ import { Box, Button, Card, Flex, Grid, Heading, Text } from '@radix-ui/themes'
 import { IssueStatusBadge, Link } from '@/app/components'
 import ReactMarkdown from "react-markdown"
 import { Pencil2Icon } from '@radix-ui/react-icons'
+import IssueDetails from './IssueDetails'
+import EditIssueButton from './EditIssueButton'
 
 interface Props {
     params: { id: string }
@@ -16,21 +18,10 @@ export default async function IssueDetailPage({ params }: Props) {
     return (
         <Grid columns={{ initial: "1", md: "2" }} gap={"5"}>
             <Box>
-                <Heading>{issue.title} </Heading>
-                <Flex className='space-x-3' my={"2"}>
-                    <IssueStatusBadge status={issue.status} />
-                    <Text>{issue.createdAt.toDateString()}</Text>
-                </Flex>
-
-                <Card className='prose' mt={"4"}>
-                    <ReactMarkdown children={issue.description} />
-                </Card>
+                <IssueDetails issue={issue} />
             </Box>
             <Box>
-                <Button>
-                    <Pencil2Icon />
-                    <Link href={`/issues/${issue.id}/edit`} >Edit Issue</Link>
-                </Button>
+                <EditIssueButton issueId={issue.id} />
             </Box>
 
         </Grid >
