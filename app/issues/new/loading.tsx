a602c772bcfb1e652f703075f15c97e603fea342
serhatacarhello <serhat.acar.tr@gmail.com>
2024-02-13 10:09:16 +0300

    Fix the loading experience

--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -1,11 +1,5 @@
-import { Box } from '@radix-ui/themes'
-import { Skeleton } from '@/app/components'
+import IssueFormSkeleton from "../_components/IssueFormSkeleton";
+export default IssueFormSkeleton;
 
-export default function LoadingNewIssuePage() {
-    return (
-        <Box className='max-w-xl'>
-            <Skeleton />
-            <Skeleton height={"20rem"} />
-        </Box>
-    )
-}
+
+
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,6 +1,9 @@
 
-import React from 'react'
-import IssueForm from '../_components/IssueForm'
+import dynamic from 'next/dynamic'
+import IssueFormSkeleton from './loading'
+
+
+const IssueForm = dynamic(()=> import("@/app/issues/_components/IssueForm"),{ssr:false, loading:()=> <IssueFormSkeleton/>})
 
 export default function NewIssuePage() {
     return (
