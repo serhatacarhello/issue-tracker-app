21985e8126e079bdbb594d88ac68b513d10d1a2b
serhatacarhello <serhat.acar.tr@gmail.com>
2024-02-15 17:29:12 +0300

    Set up Tanstack-React  Query

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { Inter } from "next/font/google";
 import { Container, Theme } from '@radix-ui/themes';
 import Navbar from "./Navbar/page";
 import AuthProvider from './auth/Provider';
+import QueryClientProvider from './QueryClientProvider';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -26,18 +27,20 @@ export default function RootLayout({
   return (
     <html lang="en" >
       <body className={inter.variable}>
-        <AuthProvider>
-          <Theme appearance="light" accentColor="violet">
-            <Navbar />
-            <main className='p-5'>
-              < Container>
+        <QueryClientProvider>
+          <AuthProvider>
+            <Theme appearance="light" accentColor="violet">
+              <Navbar />
+              <main className='p-5'>
+                < Container>
 
-                {children}
+                  {children}
 
-              </Container>
-            </main>
-          </Theme>
-        </AuthProvider>
+                </Container>
+              </main>
+            </Theme>
+          </AuthProvider>
+        </QueryClientProvider>
       </body>
     </html>
   );
