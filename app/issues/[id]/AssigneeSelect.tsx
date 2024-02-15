3f6f360b3dc0f3cc0c6d570c78eb5706717ab7c8
serhatacarhello <serhat.acar.tr@gmail.com>
2024-02-15 14:45:08 +0300

    Populate the assignee select component

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -4,15 +4,15 @@ import { Select } from "@radix-ui/themes";
 
 export default function AssigneeSelect() {
     return (
-        <Select.Root>
-            <Select.Trigger placeholder='Assign...' />
-            <Select.Content>
-                <Select.Group>
-                    <Select.Label>Suggestions</Select.Label>
-                    <Select.Item value="1">Mosh Hamedani</Select.Item>
-                    <Select.Item value="2">Serhat Acar</Select.Item>
-                </Select.Group>
-            </Select.Content>
-        </Select.Root>
+            <Select.Root>
+                <Select.Trigger placeholder='Assign...' />
+                <Select.Content>
+                    <Select.Group>
+                        <Select.Label>Suggestions</Select.Label>
+                        <Select.Item value="1">Mosh Hamedani</Select.Item>
+                        <Select.Item value="2">Serhat Acar</Select.Item>
+                    </Select.Group>
+            </Select.Content>            
+        </Select.Root>       
     );
 }
