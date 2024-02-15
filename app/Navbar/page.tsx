1b9924f89d45a8818e8d06cf0e81b0b3ca4c6fa1
serhatacarhello <serhat.acar.tr@gmail.com>
2024-02-15 07:28:12 +0300

    Refactor the nav bar

--- app/Navbar/page.tsx
+++ app/Navbar/page.tsx
@@ -9,15 +9,62 @@ import React from "react";
 import { AiFillBug } from "react-icons/ai";
 
 export default function Navbar() {
-  const currentPath = usePathname();
-  const { data: session, status } = useSession()
-  console.log(session)
 
 
-  const links = [
-    { label: "Dashboard", href: "/" },
-    { label: "Issues", href: "/issues/list" },
-  ];
+  const NavLinks = () => {
+    const currentPath = usePathname()
+    const links = [
+      { label: "Dashboard", href: "/" },
+      { label: "Issues", href: "/issues/list" },
+    ];
+
+    return (<ul className="flex  space-x-6 ">
+      {links.map((link) => (
+        <li key={link.href}>
+          <Link
+            href={link.href}
+            className={classNames({
+              "nav-link": true,
+              "!text-zinc-900": link.href === currentPath,             
+            })}
+          >
+            {link.label}
+          </Link>
+        </li>
+      ))}
+    </ul>)
+  }
+
+  const AuthStatus = () => {
+    const { data: session, status } = useSession()
+    if (status === "loading") return null
+    if (status === "unauthenticated") return <Link href={"/api/auth/signin"}>Sing In</Link>
+
+
+    return (
+      <Box>
+        <DropdownMenu.Root>
+          <DropdownMenu.Trigger>
+            <Avatar src={session!.user!.image!} fallback="?" size={"2"} radius="full" className="cursor-pointer" />
+          </DropdownMenu.Trigger>
+          <DropdownMenu.Content>
+            <DropdownMenu.Label>
+              <Text size={"2"}>
+                {session!.user!.email}
+              </Text>
+            </DropdownMenu.Label>
+            <DropdownMenu.Item>
+              <Link href={"/api/auth/signout"} >Sign Out</Link>
+            </DropdownMenu.Item>
+          </DropdownMenu.Content>
+
+        </DropdownMenu.Root>
+
+      </Box>
+    )
+  }
+
+
   return (
     <nav className="flex space-x-6 border-b shadow-sm mb-5 px-5 h-14 items-center  ">
       <Container>
@@ -26,54 +73,11 @@ export default function Navbar() {
             <Link href={"/"}>
               <AiFillBug />
             </Link>
-            <ul className="flex  space-x-6 ">
-              {links.map((link) => (
-                <li key={link.href}>
-                  <Link
-                    href={link.href}
-                    className={classNames({
-                      "text-zinc-900": link.href === currentPath,
-                      "text-zinc-500": link.href !== currentPath,
-                      "hover:text-zinc-800 transition-colors": true,
-                    })}
-                  >
-                    {link.label}
-                  </Link>
-                </li>
-              ))}
-            </ul>
+            <NavLinks />
           </Flex>
-
-          <Box>
-
-            {status === "authenticated" && (
-              <DropdownMenu.Root>
-                <DropdownMenu.Trigger>
-                  <Avatar src={session.user!.image!} fallback="?" size={"2"} radius="full" className="cursor-pointer" />
-                </DropdownMenu.Trigger>
-                <DropdownMenu.Content>
-                  <DropdownMenu.Label>
-                    <Text size={"2"}>
-                      {session.user!.email}
-                    </Text>
-                  </DropdownMenu.Label>
-                  <DropdownMenu.Item>
-                    <Link href={"/api/auth/signout"} >Sign Out</Link>
-                  </DropdownMenu.Item>
-                </DropdownMenu.Content>
-
-              </DropdownMenu.Root>
-
-            )}
-            {status === "unauthenticated" &&
-              <Link href={"/api/auth/signin"}>Sign In</Link>
-            }
-
-          </Box>
+          <AuthStatus />
         </Flex>
-
       </Container>
-
     </nav>
   );
 }
