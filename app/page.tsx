78c6a9756bf136dd759d0670c3a62c6f9e38084c
serhatacarhello <serhat.acar.tr@gmail.com>
2024-02-16 18:53:37 +0300

    Build the layout of the pagination component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
-import Image from "next/image";
+import Pagination from "./components/Pagination";
 
 export default function Home() {
-  return <h1>hello world</h1>;
+  return (
+    <>
+      <h1>hello world</h1>
+      <Pagination currentPage={2} itemCount={100} pageSize={10} />
+    </>
+  )
 }
