4c3c7c9df5f82fe3840a3faab249d8aadcf47c63
serhatacarhello <serhat.acar.tr@gmail.com>
2024-02-16 12:50:10 +0300

    Sort issues

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -26,7 +26,9 @@ export default async function IssuesPage({ searchParams, }: Props) {
 
     const status = statuses.includes(searchParams.status) ? searchParams.status : undefined;
 
-    const issues = await prisma.issue.findMany({ where: { status } });
+    const orderBy = columns.map((column) => column.value).includes(searchParams.orderBy) ? { [searchParams.orderBy]: "asc" } : undefined;
+
+    const issues = await prisma.issue.findMany({ where: { status }, orderBy });
 
     return <div>
         {/* new issue btn */}
